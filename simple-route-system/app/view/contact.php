ce22fa8386ccd02797db08b3931d0d17b9dc40a0
nely de faria <nelly.nf54@gmail.com>
2022-07-23 17:43:13 -0300

    Cria arquivo contact.php

---
+++ simple-route-system/app/view/contact.php
@@ -0,0 +1,8 @@
+<?php $this->layout('main')?>
+
+<h1>Contact</h1>
+
+<form action="/contact" method="post">
+    <input type="text" name="name" id="name" placeholder="Your name">
+    <button type="submit">Enviar</button>
+</form>
