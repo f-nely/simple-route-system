89ede456d690021d37b5c29888d380115c214561
nely de faria <nelly.nf54@gmail.com>
2022-07-24 15:27:39 -0300

    Criando novo input

--- simple-route-system/app/view/contact.php
+++ simple-route-system/app/view/contact.php
@@ -4,5 +4,6 @@
 
 <form action="/contact" method="post">
     <input type="text" name="name" id="name" placeholder="Your name">
+    <input type="email" name="email" id="email" placeholder="example@email.com">
     <button type="submit">Enviar</button>
 </form>
