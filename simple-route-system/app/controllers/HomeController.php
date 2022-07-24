0108c700e6718d9205b7d66f7d590e404b938847
nely de faria <nelly.nf54@gmail.com>
2022-07-24 15:29:26 -0300

    Alterando método index

--- simple-route-system/app/controllers/HomeController.php
+++ simple-route-system/app/controllers/HomeController.php
@@ -4,8 +4,9 @@ namespace app\controllers;
 
 class HomeController
 {
-    public function index()
+    public function index($params)
     {
+        var_dump($params->name);
         Controller::view('home');
     }
 }
